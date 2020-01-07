f52276f3ddb1c5d5528fa4e7d741d7ec217934c3
Sergei Dolgov <dolgovs@gmail.com>
2020-01-07 23:58:16 +0100

    Add test-services

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
+
+let
+
+  test-services = pkgs.writeScriptBin "test-services"
+    ''
+      echo service-a
+      ${pkgs.curl}/bin/curl http://localhost/service-a/
+      echo
+      echo service-b
+      ${pkgs.curl}/bin/curl http://localhost/service-b/
+      echo
+    '';
+
+in
 
 {
   imports = [
     ./service-a/module.nix
     ./service-b/module.nix
   ];
+
+  environment.systemPackages = [ test-services ];
 
   services.nginx = {
     enable = true;
